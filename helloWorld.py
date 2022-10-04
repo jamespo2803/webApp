647363bad6a195189cd77f57ee3f29179d97a16f
James Po <james.po3838@gmail.com>
2022-10-04 15:41:46 +1100

    website working

--- helloWorld.py
+++ helloWorld.py
@@ -1,17 +1,32 @@
-from flask import Flask
-app = Flask(__name__)
+from random import randint
+from flask import Flask, redirect, render_template,request, url_for
+app = Flask(__name__, template_folder='template')
 
-@app.route("/")
+@app.route("/", methods = ['GET','POST'])
 def index():
-    return "Home Page"
+    if request.method == 'GET':
+        return render_template('home.html')
+    if request.method == 'POST':
+        print(request.form['name_field'])
+        nam = request.form['name_field']
+        if (nam == '') :
+            return render_template('home.html')
+        return redirect(url_for('fortune', name = nam))
+    return render_template('home.html')
 
-@app.route("/hello")
-def helloWorld():
-    return "Hello World!"
+@app.route("/fortune/<string:name>/")
+def fortune(name):
+    fortunes = ["You are very lucky today. Good fortune will rain upon you.",
+    "Today is your lucky day. Any efforts you make will be rewarded generously.",
+    "You neither lucky nor unlucky today. You will be rewarded with fortune equivalent to the effort you put in.",
+    "Today is an unlucky day. Err on the side of caution in any activities you participate in."]
+    randomNumber = randint(0,(len(fortunes)-1))
+    fortune = fortunes[randomNumber]
+    return render_template('fortune.html',**locals())
 
 @app.route("/members/<string:name>/")
 def getMember(name):
-    return name
+    return render_template('test2.html',name = name)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True, host='0.0.0.0', port = 80)
